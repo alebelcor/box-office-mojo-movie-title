002da23c597a946ad224b27eb31e2ca91c98efea
alebelcor <856838+alebelcor@users.noreply.github.com>
2015-09-21 18:10:38 -0700

    Fix bug with real HTML

--- index.js
+++ index.js
@@ -2,20 +2,21 @@
 
 var cheerio = require('cheerio');
 
-function stripHTML(text) {
-  var title = text;
-
-  if (title == null) {
-    title = '';
-  }
-
+function stripHTML(title) {
   return title
     .replace('<br>', ' ')
     .trim();
 }
 
 function getMovieTitle($) {
-  return stripHTML($('td > table:nth-child(1) > tbody > tr > td:nth-child(2) > font > b').html());
+  var collection = $('td > font > b');
+  var titleElement = collection.first();
+
+  if (collection.length < 11 || titleElement == null) {
+    throw new Error('Movie title was not found.');
+  }
+
+  return stripHTML(titleElement.html());
 }
 
 module.exports = function (html) {
